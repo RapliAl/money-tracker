4aa56b5a591dbe4e15de8ae051b43673d3b53895
MUHAMMAD RAFLI AL ISFAHANI <2311102441245@umkt.ac.id>
2026-02-23 03:27:05 +0800

    feat: Modified File & add Darkmode Feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import {Button} from "@/src/components/ui/button";
+import Link from "next/dist/client/link";
+import {DarkModeToggle} from "@/src/components/common/darkmode-toggle";
+
 export default function Home() {
   return (
-      <div>
-            <h1 className="font-bold"> Welcome to this App </h1>
-
+      <>
+      <DarkModeToggle />
+      <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
+            <h1 className="font-bold items-center justify-between"> Welcome to this App </h1>
+          <Button variant="outline" className="bg-blue-200 hover:bg-blue-400 text-white-500 hover:text-white-600">
+              <Link href="/dashboard">
+                  <p className="font-semibold"> Go to Dashboard Panel </p>
+              </Link>
+          </Button>
       </div>
+      </>
   )
 }
